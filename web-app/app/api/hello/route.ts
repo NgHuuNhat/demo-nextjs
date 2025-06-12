4f872a5e680f52b4576fb96f296f99ff19517340
NgHuuNhat <nhat200901@gmail.com>
2025-06-12 13:19:10 +0700

    update

--- web-app/app/api/hello/route.ts
+++ web-app/app/api/hello/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
 
 export function GET() {
-  return NextResponse.json({ message: 'Hello from API!' });
+  return NextResponse.json({ message: 'success' });
 }
--- web-app/app/page.tsx
+++ web-app/app/page.tsx
@@ -1,37 +1,33 @@
 // call api Client Component
-// 'use client';
-// import { useEffect, useState } from 'react';
+'use client';
+import { useEffect, useState } from 'react';
 
-import { getBaseUrl } from "./lib/getBaseUrl";
+export default function Home() {
+  const [message, setMessage] = useState('');
 
-// export default function Home() {
-//   const [message, setMessage] = useState('');
-
-//   useEffect(() => {
-//     fetch('/api/hello')
-//       .then(res => res.json())
-//       .then(data => setMessage(data.message));
-//   }, []);
-
-//   return (
-//     <main style={{ padding: 20 }}>
-//       <h1>Welcome to Next.js 13+</h1>
-//       <p>API says: {message}</p>
-//     </main>
-//   );
-// }
-
-// call api Server Component
-export default async function Home() {
-  const res = await fetch('https://demo-nextjs-one.vercel/api/hello', { cache: 'no-store' });
-  const { message } = await res.json();
-  //  const res = await fetch(`${getBaseUrl()}/api/hello`, { cache: 'no-store' });
-  // const { message } = await res.json();
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(res => res.json())
+      .then(data => setMessage(data.message));
+  }, []);
 
   return (
     <main style={{ padding: 20 }}>
       <h1>Welcome to Next.js 13+</h1>
-      <p>API says: {message}</p>
+      <p>call api: {message}</p>
     </main>
   );
 }
+
+// call api Server Component
+// export default async function Home() {
+//   const res = await fetch('http://localhost:3001/api/hello', { cache: 'no-store' });
+//   const { message } = await res.json();
+
+//   return (
+//     <main style={{ padding: 20 }}>
+//       <h1>Welcome to Next.js 13+</h1>
+//       <p>call api: {message}</p>
+//     </main>
+//   );
+// }
