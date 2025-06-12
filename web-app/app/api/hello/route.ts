92641f924be0b4278c0722b1006164baf13f2ce7
NgHuuNhat <nhat200901@gmail.com>
2025-06-12 12:58:55 +0700

    update call api server component

--- web-app/app/api/hello/route.ts
+++ web-app/app/api/hello/route.ts
@@ -1,6 +1,5 @@
-// app/api/hello/route.js
 import { NextResponse } from 'next/server';
 
-export async function GET() {
-  return NextResponse.json({ message: 'Hello from Next.js 13 API!' });
+export function GET() {
+  return NextResponse.json({ message: 'Hello from API!' });
 }
--- web-app/app/page.tsx
+++ web-app/app/page.tsx
@@ -1,15 +1,28 @@
-'use client';
+// call api Client Component
+// 'use client';
+// import { useEffect, useState } from 'react';
 
-import { useEffect, useState } from 'react';
+// export default function Home() {
+//   const [message, setMessage] = useState('');
 
-export default function Home() {
-  const [message, setMessage] = useState('');
+//   useEffect(() => {
+//     fetch('/api/hello')
+//       .then(res => res.json())
+//       .then(data => setMessage(data.message));
+//   }, []);
 
-  useEffect(() => {
-    fetch('/api/hello')
-      .then(res => res.json())
-      .then(data => setMessage(data.message));
-  }, []);
+//   return (
+//     <main style={{ padding: 20 }}>
+//       <h1>Welcome to Next.js 13+</h1>
+//       <p>API says: {message}</p>
+//     </main>
+//   );
+// }
+
+// call api Server Component
+export default async function Home() {
+  const res = await fetch('http://localhost:3001/api/hello', { cache: 'no-store' });
+  const { message } = await res.json();
 
   return (
     <main style={{ padding: 20 }}>
